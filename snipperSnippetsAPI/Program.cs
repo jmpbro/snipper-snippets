276e529a1ab9f34a237e52d55593c05dd171dd18
Jolene <jolene.m.p.brown@gmail.com>
2025-01-20 11:46:37 +0000

    Model User.cs added and changes made to Program.cs

--- snipperSnippetsAPI/Program.cs
+++ snipperSnippetsAPI/Program.cs
@@ -1,8 +1,19 @@
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using SnipperSnippet.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 var snippets = new List<Snippet>();
 
